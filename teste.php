f0d6486d706158cbf8e1627eec310109e5e78d5f
Karoliny Oliveira <karolinyoliveira@being.com.br>
2022-08-17 14:47:42 -0300

    primeiro commit

---
+++ teste.php
@@ -0,0 +1,52 @@
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+    </head>
+<body>
+    <table border="1" align="center">
+    <tr>
+        <td colspan="3"><img src="imagem/BANNER.png" alt="imagem do cabeçalho"/></td>
+    </tr>
+    <tr>
+        
+       <td rowspan="1"><img src="imagem/logo.png" alt="logo da loja"/>
+    </td> 
+    </tr>
+    <p>
+        <table style="text-align:center ;">
+    <tr>
+        <th style=color:DeepPink> A fratello já nasceu com tradição,</th>
+        <br></br>
+        </tr>
+        <tr>
+        <th style="text-align:center">sendo do mesmo grupo da familia La Paleta,</th>
+        <br>
+        <tr>
+            <th style="text-align:center">que atua no mercado desde 2014.</th>
+        </tr>
+      </tr>
+    </table>
+    <br>
+    <br>
+</table>
+<table>
+<tr>
+    <td rowspan="2"><img scr="imagem/imagem1c1.png"></td>
+    <br>
+    <td rowspan="2"><img src="imagem/imagem2c1.png"></td>
+    
+</tr>
+<table>
+<tr>
+    <td rowspan="2"><img src="imagem/imagem1c2.png"></td>
+<br>
+    <td rowspan="2"><img src="imagem/imagem2c2.png"></td>
+<br>
+    <td rowspan="2"><img src="imagem/imagem3c2.png"></td>
+</tr>
+</table>
+</table>
+</body>
+</html>
